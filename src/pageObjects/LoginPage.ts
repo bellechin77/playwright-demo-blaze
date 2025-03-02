38e0745d59b6e9205a8e69699c7ba6bb19bf3aa0
Belle Pui-Leng Chin <pui.leng@netcentric.biz>
2025-03-02 18:24:35 +0100

    fix locators for login

--- src/pageObjects/LoginPage.ts
+++ src/pageObjects/LoginPage.ts
@@ -5,8 +5,8 @@ export class LoginPage {
 
   async login(username: string, password: string) {
       await this.page.getByRole('link', { name: 'Log in' }).click();
-      await this.page.getByRole('textbox', { name: 'loginusername' }).fill(username);
-      await this.page.getByRole('textbox', { name: 'loginpassword' }).fill(password);
+      await this.page.locator('#loginusername').fill(username);
+      await this.page.locator('#loginpassword').fill(password);
       await this.page.getByRole('button', { name: 'Log in' }).click();
   }
 }
