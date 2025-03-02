49e30465d540b53b262b2b93e3cb674f956722ba
Belle Pui-Leng Chin <pui.leng@netcentric.biz>
2025-03-02 18:04:40 +0100

    fix syntax error

--- src/pageObjects/SignUpPage.ts
+++ src/pageObjects/SignUpPage.ts
@@ -4,7 +4,7 @@ export class SignUpPage {
     async signUp(username: string, password: string) {
       await this.page.getByRole('link', { name: 'Sign up' }).click();
       await this.page.getByRole('textbox', { name: 'Username' }).fill(username);
-      await this.page.getByPRole('textbox', { name: 'Password' }).fill(password);
+      await this.page.getByRole('textbox', { name: 'Password' }).fill(password);
       await this.page.getByRole('button', { name: 'Sign up' }).click();
     }
   }
