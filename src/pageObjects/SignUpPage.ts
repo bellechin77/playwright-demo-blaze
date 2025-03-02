8a4a32cdc44cf6dede51b031067824cb16e3c217
Belle Pui-Leng Chin <pui.leng@netcentric.biz>
2025-03-02 18:02:24 +0100

    rectify syntax error on locators

--- src/pageObjects/SignUpPage.ts
+++ src/pageObjects/SignUpPage.ts
@@ -3,8 +3,8 @@ export class SignUpPage {
   
     async signUp(username: string, password: string) {
       await this.page.getByRole('link', { name: 'Sign up' }).click();
-      await this.page.getByRole('textbox').fill(username);
-      await this.page.getByPRole('textbox').fill(password);
+      await this.page.getByRole('textbox', { name: 'Username' }).fill(username);
+      await this.page.getByPRole('textbox', { name: 'Password' }).fill(password);
       await this.page.getByRole('button', { name: 'Sign up' }).click();
     }
   }
