84c09ecc614e710af454140ec0df31750e1f6da7
Belle Pui-Leng Chin <pui.leng@netcentric.biz>
2025-03-02 18:39:20 +0100

    fix locator for product selector

--- src/pageObjects/CartPage.ts
+++ src/pageObjects/CartPage.ts
@@ -2,7 +2,7 @@ export class CartPage {
     constructor(private page: Page) {}
   
     async addToCart(productName: string) {  
-      await this.page.getByText(productName).click();
+      await this.page.getByRole('link', { name: productName }).click();
       await this.page.getByRole('button', { name: 'Add to cart' }).click();
       await this.page.waitForEvent('dialog'); // Handle alert popup
       }
