f14bee1a03f5fb065b9455228b4cba14ef86a58a
Belle Pui-Leng Chin <pui.leng@netcentric.biz>
2025-03-02 17:59:02 +0100

    fix locator for username & password

--- src/pageObjects/SignUpPage.ts
+++ src/pageObjects/SignUpPage.ts
@@ -3,8 +3,8 @@ export class SignUpPage {
   
     async signUp(username: string, password: string) {
       await this.page.getByRole('link', { name: 'Sign up' }).click();
-      await this.page.getByPlaceholder('Username').fill(username);
-      await this.page.getByPlaceholder('Password').fill(password);
+      await this.page.getByRole('textbox').fill(username);
+      await this.page.getByPRole('textbox').fill(password);
       await this.page.getByRole('button', { name: 'Sign up' }).click();
     }
   }
